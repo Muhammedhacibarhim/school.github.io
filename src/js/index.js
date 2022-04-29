9340f365fb18b3311ff3a0eb77f2e3f82a607645
Asmar <alzlam150@gmail.com>
2022-04-29 21:29:57 +0300

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
 import '@laylazi/bootstrap-rtl/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min.js';
 import '../sass/style.scss';
